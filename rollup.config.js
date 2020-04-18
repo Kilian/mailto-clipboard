7852ffae2238d0b8a16bc8a6ef489f653f66a50a
Kilian Valkhof <kilian@kilianvalkhof.com>
2020-04-18 13:44:32 +0200

    dont ship cjs

--- rollup.config.js
+++ rollup.config.js
@@ -31,7 +31,6 @@ export default [
     input: "src/index.js",
     external: [],
     output: [
-      { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" }
     ],
     plugins: [
